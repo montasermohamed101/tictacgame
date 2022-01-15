8cebc555eef70143121256eb0231c67c6cd6dc4b
montasermohamed101 <montasermohamed101@gmail.com>
2022-01-15 03:11:35 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tictacgame/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF00061a),
+        shadowColor: const Color(0xFF001456),
+        splashColor: const Color(0xFF4169e8),
+      ),
+      home: HomePage(),
+    );
+  }
+}
